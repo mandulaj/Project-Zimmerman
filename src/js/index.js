ec28e71c08163fe192cfbb767459ad704e3efd66
Jakub Mandula <jakub.aludnam@gmail.com>
2015-01-08 18:16:05 +0100

    organize js handler

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,23 @@
 (function($) {
   'use strict';
 
-  $(document).ready(function() {
+  function EventHandler() {
+    var navbarOpen = false;
 
-    var last = 0;
-    $(this).on("mousemove", function(e) {
-      if (e.timeStamp > last + 500) {
-        console.log(e)
-        var w = window.innerWidth;
-        var h = window.innerHeight;
-        var wP = 40 * (e.clientX / w * 0.15 + 0.75);
-        var hP = 40 * (e.clientY / h * 0.15 + 0.75);
-        last = e.timeStamp;
-        //$(".background").css("background-position", wP + "% " + hP + "%");
+    $(".nav-toggle").click(function() {
+      if (navbarOpen) {
+        navbarOpen = false;
+
+      } else {
+        navbarOpen = true;
       }
     });
+  }
+
+
+
+  $(document).ready(function() {
+    var handler = new EventHandler();
+
   });
 })($);
