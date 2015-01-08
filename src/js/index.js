d06d379aea64e8a8fd0b675b19cbf468bc9325e2
Jakub Mandula <jakub.aludnam@gmail.com>
2015-01-08 20:46:30 +0100

    implement nav toggle

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,40 @@
   'use strict';
 
   function EventHandler() {
-    var navbarOpen = false;
+    var self = this;
+    this.navbarOpen = false;
 
-    $(".nav-toggle").click(function() {
-      if (navbarOpen) {
-        navbarOpen = false;
 
-      } else {
-        navbarOpen = true;
-      }
-    });
+    $(".nav-toggle").click(this.toggleNav.bind(this));
+    $(document).click(this.hiddeNav.bind(this));
   }
+
+
+  EventHandler.prototype.toggleNav = function() {
+    var button = $(".nav-toggle");
+    var menu = $("nav");
+    if (this.navbarOpen) {
+      this.navbarOpen = false;
+      menu.removeClass("showNav");
+      button.removeClass("dropped");
+    } else {
+      this.navbarOpen = true;
+      menu.addClass("showNav");
+      button.addClass("dropped");
+    }
+  };
+  EventHandler.prototype.hiddeNav = function(e) {
+    var button = $(".nav-toggle");
+    var menu = $("nav");
+    var target = $(e.target);
+    if (e.target == button.get(0) || e.target == menu.get(0) || button.find(target).length !== 0 || menu.find(target).length !== 0) {
+      return;
+    } else {
+      this.navbarOpen = false;
+      menu.removeClass("showNav");
+      button.removeClass("dropped");
+    }
+  };
 
 
 
